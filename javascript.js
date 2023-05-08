c4539373de6a6963b949dee9e894b0580df4a287
gusty <gustavodiorlando@gmail.com>
2023-05-08 15:04:50 -0300

    Define play round function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let escolha = Math.floor(Math.random() * 3) // Gerar número 0, 1, 2
     // 0 = Pedra, 1 = Papel, 2 = Tesoura
     if (escolha === 0) {
@@ -7,5 +7,24 @@ function getComputerChoice () {
         return 'papel'
     } else if (escolha === 2) {
         return 'tesoura'
+    }
+}
+
+function round(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    if playerSelection === computerSelection {
+        return 'empate!'
+    } else if playerSelection === 'pedra' && computerSelection === 'papel' {
+        return 'você perdeu! Papel > Pedra'
+    } else if playerSelection === 'pedra' && computerSelection === 'tesoura' {
+        return 'você ganhou! Pedra > Tesoura'
+    } else if playerSelection === 'papel' && computerSelection === 'pedra' {
+        return 'você ganhou! Papel > Pedra'
+    } else if playerSelection === 'papel' && computerSelection === 'tesoura' {
+        return 'você perdeu! Tesoura > Papel'
+    } else if playerSelection === 'tesoura' && computerSelection === 'pedra' {
+        return 'você perdeu! Pedra > Tesoura'
+    } else if playerSelection === 'tesoura' && computerSelection === 'papel' {
+        return 'você ganhou! Tesoura > Papel'
     }
 }
