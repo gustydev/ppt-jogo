be4558fe91368f7700e3896c8bb50d4c1871ecc9
gusty <gustavodiorlando@gmail.com>
2023-05-08 14:45:18 -0300

    Define computer choice function

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice () {
+    let escolha = Math.floor(Math.random() * 3) // Gerar número 0, 1, 2
+    // 0 = Pedra, 1 = Papel, 2 = Tesoura
+    if (escolha === 0) {
+        return 'pedra'
+    } else if (escolha === 1) {
+        return 'papel'
+    } else if (escolha === 2) {
+        return 'tesoura'
+    }
+}
