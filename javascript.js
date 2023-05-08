b02b78a004989edb97e836c80d9f168745201857
gusty <gustavodiorlando@gmail.com>
2023-05-08 15:12:02 -0300

    Fix syntax errors in round function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+const playerSelection = 'pedra'
+const computerSelection = getComputerChoice()
+console.log(round(playerSelection, computerSelection))
+
 function getComputerChoice() {
     let escolha = Math.floor(Math.random() * 3) // Gerar número 0, 1, 2
     // 0 = Pedra, 1 = Papel, 2 = Tesoura
@@ -11,20 +15,19 @@ function getComputerChoice() {
 }
 
 function round(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    if playerSelection === computerSelection {
+    if (playerSelection === computerSelection) {
         return 'empate!'
-    } else if playerSelection === 'pedra' && computerSelection === 'papel' {
+    } else if (playerSelection === 'pedra' && computerSelection === 'papel') {
         return 'você perdeu! Papel > Pedra'
-    } else if playerSelection === 'pedra' && computerSelection === 'tesoura' {
+    } else if (playerSelection === 'pedra' && computerSelection === 'tesoura') {
         return 'você ganhou! Pedra > Tesoura'
-    } else if playerSelection === 'papel' && computerSelection === 'pedra' {
+    } else if (playerSelection === 'papel' && computerSelection === 'pedra') {
         return 'você ganhou! Papel > Pedra'
-    } else if playerSelection === 'papel' && computerSelection === 'tesoura' {
+    } else if (playerSelection === 'papel' && computerSelection === 'tesoura') {
         return 'você perdeu! Tesoura > Papel'
-    } else if playerSelection === 'tesoura' && computerSelection === 'pedra' {
+    } else if (playerSelection === 'tesoura' && computerSelection === 'pedra') {
         return 'você perdeu! Pedra > Tesoura'
-    } else if playerSelection === 'tesoura' && computerSelection === 'papel' {
+    } else if (playerSelection === 'tesoura' && computerSelection === 'papel') {
         return 'você ganhou! Tesoura > Papel'
     }
 }
